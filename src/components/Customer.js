8db9559ea2982e5a01b99517124224610520ccce
Jeremy <jsoon049@uottawa.ca>
2021-05-27 18:48:23 -0400

    Initial commit

---
+++ src/components/Customer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Customer = () => {
+    return (
+        <div>
+            <h1>Customer</h1>
+        </div>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div className="container">
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Customer</Link>
+        </li>
+        <li>
+          <Link to="/product">Product</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
